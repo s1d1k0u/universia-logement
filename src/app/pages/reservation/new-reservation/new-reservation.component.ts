8514e3ce9cc0c2c90f36612304660b3592049f5b
s1d1k0u <as.sidikousoumana@gmail.com>
2025-02-14 12:31:50 +0100

    Implement reactive form for new reservation with validation and error handling

--- src/app/pages/reservation/new-reservation/new-reservation.component.ts
+++ src/app/pages/reservation/new-reservation/new-reservation.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-reservation',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-reservation.component.html',
-  styleUrl: './new-reservation.component.css'
+  styleUrl: './new-reservation.component.css',
 })
 export class NewReservationComponent {
+  studentForm: FormGroup = new FormGroup({
+    registrationNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-z]{3}[A-Z][0-9]{8}$'),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
+  get registrationNumber() {
+    return this.studentForm.get('registrationNumber');
+  }
+
+  get email() {
+    return this.studentForm.get('email');
+  }
+
+  onSubmit() {
+    console.log(this.studentForm.value);
+  }
 }
