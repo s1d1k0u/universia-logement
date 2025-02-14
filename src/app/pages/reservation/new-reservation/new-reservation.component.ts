a79561ff35f3ed883b7514b213425a64cd6e0de1
s1d1k0u <as.sidikousoumana@gmail.com>
2025-02-14 09:27:13 +0100

    Add new reservation page

---
+++ src/app/pages/reservation/new-reservation/new-reservation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-reservation',
+  imports: [],
+  templateUrl: './new-reservation.component.html',
+  styleUrl: './new-reservation.component.css'
+})
+export class NewReservationComponent {
+
+}
