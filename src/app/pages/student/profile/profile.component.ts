a64791f95f209f8cf01ca95e9b9b9cabb3c0b595
s1d1k0u <as.sidikousoumana@gmail.com>
2025-02-14 18:10:02 +0100

    Add profile component with reactive form for user information update

---
+++ src/app/pages/student/profile/profile.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+export class ProfileComponent {
+  profileForm: FormGroup = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    genre: new FormControl('', [Validators.required]),
+  });
+  isSubmitting = false;
+
+  constructor() {
+    this.profileForm.patchValue({
+      firstName: 'John',
+      lastName: 'Doe',
+    });
+  }
+
+  get firstName() {
+    return this.profileForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.profileForm.get('lastName');
+  }
+
+  get genre() {
+    return this.profileForm.get('genre');
+  }
+
+  onSubmit() {
+    //@TODO: Implement onSubmit method
+    if (!this.profileForm.invalid) {
+      this.isSubmitting = true;
+      console.log(this.profileForm.value);
+      this.isSubmitting = false;
+      return;
+    }
+  }
+}
