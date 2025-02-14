3fb723bd386a4ac5eb161738b2592f53c93bb7b3
s1d1k0u <as.sidikousoumana@gmail.com>
2025-02-14 18:09:32 +0100

    Enhance default layout with user profile dropdown and logout functionality

--- src/app/layouts/default-layout/default-layout.component.ts
+++ src/app/layouts/default-layout/default-layout.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-default-layout',
-  imports: [RouterOutlet, RouterLink, SidebarComponent],
+  imports: [RouterOutlet, CommonModule, SidebarComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.css',
 })
 export class DefaultLayoutComponent {
+  isMenuOpen = false;
   isMobileMenuOpen = false;
+
+  logout() {
+    alert('logout');
+  }
 }
